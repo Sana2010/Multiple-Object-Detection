c49eda6e22452fd52a2500cf3549dfc90df77889
Sana2010 <78859417+Sana2010@users.noreply.github.com>
2021-07-21 19:04:25 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+function go_tent()
+{
+window.location = "tent.html";
+}
+
+function go_shoecase()
+{
+window.location = "showcase.html";
+}
+
+function go_shoerack()
+{
+window.location = "shoerack.html";
+}
